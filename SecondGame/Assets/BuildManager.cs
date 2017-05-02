1da7ef88edbfc16221500475b374897b0a6c9e98
ligrima <lianagrima@gmail.com>
2017-05-02 14:31:45 +0200

    6 ready, enemies, bullets, turrets as much as you want
    enemies are being killed by the bullets, can add as much as you want -
    turrets, can be placed where ever you like, and if there is one placed
    there already, it will tell the player that that node is already taken

---
+++ SecondGame/Assets/BuildManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManagerin scene!");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+    private GameObject turretToBuild;
+
+    public GameObject GetTurrentToBuild ()
+    {
+        return turretToBuild;
+    }
+}
---
+++ SecondGame/Assets/Node.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    //create variable hoverColor
+    public Color hoverColor;
+
+    //create variable positionOffset so that we can arrange the position
+    public Vector3 positionOffset;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+
+    // get colour of the node, at the beginning of the game, store in startColor
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+
+    }
+
+    //on click while hovering, if turret is not empty, you cant build
+    private void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Can't build there! - TODO: Display on screen.");
+            return;
+        }
+
+        //build a turrent
+        GameObject turretToBuild = BuildManager.instance.GetTurrentToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    //when mouse hovers over the node, change colour to the hover colour
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    //when mouse is not hovering on the node, change colour to the start colour
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
